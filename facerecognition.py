10a67950f82057bf43fd3e8ae49c61930e558c9b
jhon19.2 <jhon19.2@outlook.com>
2021-12-04 19:14:20 -0400

    updating face recognition

---
+++ facerecognition.py
@@ -0,0 +1,42 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+
+def FaceRecognition(cap):
+    mp_face_detection = mp.solutions.face_detection
+    mp_drawing = mp.solutions.drawing_utils
+
+    ptime = 0
+    with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+        while True:
+            success, image = cap.read()
+            if not success:
+                print("Failed to capture image")
+                break
+            image.flags.writeable = False
+            image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+            results = face_detection.process(image)
+
+            image.flags.writeable = True
+            image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+
+            if results.detections:
+                for detection in results.detections:
+                    #mp_drawing.draw_detection(image, detection)
+                    bboxC = detection.location_data.relative_bounding_box
+                    ih, iw, ic = image.shape
+                    bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
+                    final_img = cv.rectangle(image, bbox, (0, 200, 0), 2)
+                    cv.putText(final_img, 'Jhon',  (int(bboxC.xmin * iw), int(bboxC.ymin * ih)), cv.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+                ctime = time.time()
+                fps = 1 / (ctime - ptime)
+                ptime = ctime
+                cv.putText(image, "press q to exit".format(fps), (10, 470), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 100), 2)
+                cv.imshow("Face Detection", image)
+
+
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                cv.destroyAllWindows()
+                break                
+    cap.release()
+
--- main.py
+++ main.py
@@ -1,32 +1,62 @@
 from tkinter import *
+import cv2 as cv
+from PIL import Image, ImageTk
+import mediapipe as mp
+
+from facerecognition import FaceRecognition
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
 
 
-window= Tk()
+window = Tk()
+cap = cv.VideoCapture(0)
 window.title('Computer Vision')
 window.geometry('900x400')
 
 
+frame1 = Frame(window, width=100, height=100, highlightbackground='blue',
+               highlightthickness=3)
+frame1.grid(row=0, column=0, padx=30, pady=40, ipadx=195, ipady=110)
 
-frame1 = Frame(window, width=100,highlightbackground='blue',highlightthickness= 3)
-frame1.grid(row=0, column=0, padx=30,pady= 40, ipadx= 195,ipady=140)
-
-label1= Label(frame1, text="Camera",fg='red', font=(18))
-label1.place(x=215, y=3, anchor="center")
+label1 = Label(frame1, text="Camera", fg='red', font=(18))
+label1.place(x=250, y=80, anchor="center")
 
 
-frame2 = Frame(window,width=100, highlightbackground='blue', highlightthickness=3)
-frame2.grid(row=0,column=1, padx=100, pady=10, ipadx=60, ipady=145)
+def show_frames():
+    success, image = cap.read()
+    cv2image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    label1.imgtk = imgtk
+    label1.configure(image=imgtk)
+    label1.after(20, show_frames)
 
-frame3 = Frame(window,width=100, highlightbackground='blue', highlightthickness=2)
-frame3.grid(row=0,column=1, padx=10, pady=10, ipadx=40, ipady=100,)
-label2= Label(frame3, text="Result List",fg='red', font=(18))
+def detect_face():
+    # return FaceRecognition()
+    FaceRecognition(cap)
+    # window.destroy()
+
+
+frame2 = Frame(window, width=100, highlightbackground='blue',
+               highlightthickness=3)
+frame2.grid(row=0, column=1, padx=100, pady=10, ipadx=60, ipady=145)
+
+frame3 = Frame(window, width=100, highlightbackground='blue',
+               highlightthickness=2)
+frame3.grid(row=0, column=1, padx=10, pady=10, ipadx=40, ipady=100,)
+label2 = Label(frame3, text="Result List", fg='red', font=(18))
 label2.place(x=85, y=6, anchor="center")
 
 
-frame4 = Frame(window,width=100, highlightbackground='blue', highlightthickness=2)
-frame4.grid(row=0,column=1, padx=10, pady=10, ipadx=40, ipady=50,)
+frame4 = Frame(window, width=100, highlightbackground='blue',
+               highlightthickness=2)
+frame4.grid(row=0, column=1, padx=10, pady=10, ipadx=40, ipady=50,)
 
+btn = Button(frame1, text="Start face recognition", height=2,
+                command=detect_face )
+btn.place(x=180, y=260)
 
-
-
+show_frames()
 window.mainloop()
+
